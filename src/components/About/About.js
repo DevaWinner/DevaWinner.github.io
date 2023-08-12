5f6a497236496429169a876686c07b05eb681ac1
DevaWinner <winneraaniekan@gmail.com>
2023-08-12 18:16:29 +0100

    Add about component

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about">
+      <h1>About</h1>
+      <p>
+        This is a simple React app that uses React Router to navigate between
+        pages.
+      </p>
+    </div>
+  );
+}
+
+export default About;
