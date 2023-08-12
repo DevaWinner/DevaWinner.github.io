c45b453d1bf2cc1af2f77ab3da67fa1f17f49b6b
DevaWinner <winneraaniekan@gmail.com>
2023-08-12 18:15:06 +0100

    Format default structure

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Nav/Nav';
+import Homepage from './components/Homepage/Homepage';
+import About from './components/About/About';
 import './App.css';
-import TypingText from './components/TypingText';
 
 function App() {
-  const textToType = "DevWinner.";
-
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <TypingText text={textToType} />
-        <p>
-          Portfolio under development <br /> Coming soon...
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/DevaWinner"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Connect with me on Github
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navigation />
+
+      <Routes>
+        <Route className="navbar-link" path="/" element={<Homepage />} />
+        <Route className="navbar-link" path="/About" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
