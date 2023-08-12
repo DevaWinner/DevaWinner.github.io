23995fb6dcc13d494852b22648842fdc42cc89a1
DevaWinner <winneraaniekan@gmail.com>
2023-08-12 18:16:17 +0100

    Add homepage component

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <h1>Homepage</h1>
+      <p>
+        This is a simple React app that uses React Router to navigate between
+        pages.
+      </p>
+    </div>
+  );
+}
+
+export default Homepage;
